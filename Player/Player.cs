690ddacc63c181a4f988d460132945854683effe
Dustin M. Eastway <Dustin.Eastway@gmail.com>
2021-12-19 12:21:42 -0500

    add player movement and idle animations

--- Player/Player.cs
+++ Player/Player.cs
@@ -2,6 +2,8 @@ using Godot;
 using System;
 
 public class Player : KinematicBody2D {
+	private AnimationPlayer _AnimationPlayer;
+
 	/// <summary>How quickly the player's <see cref="Velocity"> gets up to <see cref="MaxSpeed"></summary>
 	public float Acceleration {
 		get => 500;
@@ -28,11 +30,23 @@ public class Player : KinematicBody2D {
 		).Normalized();
 
 		if (inputVector != Vector2.Zero) {
+			if (inputVector.x > 0) {
+				_AnimationPlayer.Play("Run");
+			} else {
+				_AnimationPlayer.Play("RunLeft");
+			}
+
 			Velocity = Velocity.MoveToward(inputVector * MaxSpeed, Acceleration * delta);
 		} else {
+			_AnimationPlayer.Play("IdleRight");
 			Velocity = Velocity.MoveToward(Vector2.Zero, Friction * delta);
 		}
 
 		Velocity = MoveAndSlide(Velocity);
+	}
+
+	/// <inheritdoc />
+	public override void _Ready() {
+		_AnimationPlayer = GetNode<AnimationPlayer>("AnimationPlayer");
 	}
 }
