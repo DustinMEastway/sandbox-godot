ece5c8e3948508aec19025c004641770cd5c35f0
Dustin M. Eastway <Dustin.Eastway@gmail.com>
2021-12-19 10:52:33 -0500

    add player acceleration and friction

--- Player/Player.cs
+++ Player/Player.cs
@@ -2,14 +2,37 @@ using Godot;
 using System;
 
 public class Player : KinematicBody2D {
-	private Vector2 _Velocity = Vector2.Zero;
+	/// <summary>How quickly the player's <see cref="Velocity"> gets up to <see cref="MaxSpeed"></summary>
+	public float Acceleration {
+		get => 500;
+	}
 
+	/// <summary>How quickly the player's <see cref="Velocity"> slows down to 0.</summary>
+	public float Friction {
+		get => Acceleration;
+	}
+
+	/// <summary>Max distance per second the player can travel at.</summary>
+	public float MaxSpeed {
+		get => 80;
+	}
+
+	/// <summary>Current distance per second the player is travelling at.</summary>
+	public Vector2 Velocity { get; private set; }
+
+	/// <inheritdoc />
  	public override void _PhysicsProcess(float delta) {
-		_Velocity = new Vector2(
+		var inputVector = new Vector2(
 			Input.GetActionStrength("ui_right") - Input.GetActionStrength("ui_left"),
 			Input.GetActionStrength("ui_down") - Input.GetActionStrength("ui_up")
-		);
+		).Normalized();
 
-		MoveAndCollide(_Velocity);
+		if (inputVector != Vector2.Zero) {
+			Velocity = Velocity.MoveToward(inputVector * MaxSpeed, Acceleration * delta);
+		} else {
+			Velocity = Velocity.MoveToward(Vector2.Zero, Friction * delta);
+		}
+
+		MoveAndCollide(Velocity * delta);
 	}
 }
