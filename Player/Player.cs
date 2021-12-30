0af46e87e5131f39af9d5dcc4f0978fa1a5b711c
Dustin M. Eastway <Dustin.Eastway@gmail.com>
2021-12-30 10:47:33 -0500

    add roll state to player

--- Player/Player.cs
+++ Player/Player.cs
@@ -10,6 +10,7 @@ public enum PlayerState {
 public class Player : KinematicBody2D {
 	private AnimationTree _AnimationTree;
 	private AnimationNodeStateMachinePlayback _AnimationTreeState;
+	private Vector2 _InputDirection = Vector2.Zero;
 	private PlayerState _State = PlayerState.Move;
 
 	/// <summary>How quickly the player's <see cref="Velocity"> gets up to <see cref="MaxSpeed"></summary>
@@ -27,20 +28,26 @@ public class Player : KinematicBody2D {
 		get => 80;
 	}
 
+	public float MaxRollSpeed {
+		get => MaxSpeed * 1.5f;
+	}
+
 	/// <summary>Current distance per second the player is travelling at.</summary>
 	public Vector2 Velocity { get; private set; }
 
 	/// <inheritdoc />
  	public override void _PhysicsProcess(float delta) {
-		 switch (_State) {
+		switch (_State) {
 			case PlayerState.Attack:
 				StateAttack(delta);
 				break;
 			case PlayerState.Move:
 			 	StateMove(delta);
 				break;
-
-		 }
+			case PlayerState.Roll:
+				StateRoll(delta);
+				break;
+		}
 	}
 
 	/// <inheritdoc />
@@ -60,17 +67,18 @@ public class Player : KinematicBody2D {
 	}
 
 	private void StateMove(float delta) {
-		var inputVector = new Vector2(
+		_InputDirection = new Vector2(
 			Input.GetActionStrength("ui_right") - Input.GetActionStrength("ui_left"),
 			Input.GetActionStrength("ui_down") - Input.GetActionStrength("ui_up")
 		).Normalized();
 
-		if (inputVector != Vector2.Zero) {
-			_AnimationTree.Set("parameters/Attack/blend_position", inputVector);
-			_AnimationTree.Set("parameters/Idle/blend_position", inputVector);
-			_AnimationTree.Set("parameters/Run/blend_position", inputVector);
+		if (_InputDirection != Vector2.Zero) {
+			_AnimationTree.Set("parameters/Attack/blend_position", _InputDirection);
+			_AnimationTree.Set("parameters/Idle/blend_position", _InputDirection);
+			_AnimationTree.Set("parameters/Roll/blend_position", _InputDirection);
+			_AnimationTree.Set("parameters/Run/blend_position", _InputDirection);
 			_AnimationTreeState.Travel("Run");
-			Velocity = Velocity.MoveToward(inputVector * MaxSpeed, Acceleration * delta);
+			Velocity = Velocity.MoveToward(_InputDirection * MaxSpeed, Acceleration * delta);
 		} else {
 			_AnimationTreeState.Travel("Idle");
 			Velocity = Velocity.MoveToward(Vector2.Zero, Friction * delta);
@@ -80,6 +88,14 @@ public class Player : KinematicBody2D {
 
 		if (Input.IsActionJustPressed("attack")) {
 			_State = PlayerState.Attack;
+		} else if (Input.IsActionJustPressed("roll") && _InputDirection != Vector2.Zero) {
+			_State = PlayerState.Roll;
 		}
+	}
+
+	private void StateRoll(float delta) {
+		_AnimationTreeState.Travel("Roll");
+		Velocity = _InputDirection * MaxRollSpeed;
+		Velocity = MoveAndSlide(Velocity);
 	}
 }
