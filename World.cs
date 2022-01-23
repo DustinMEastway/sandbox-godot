b00c7116cec5c276ecb3d1f2c993a0a39ccb6d7d
Dustin M. Eastway <Dustin.Eastway@gmail.com>
2022-01-23 00:39:07 -0500

    spawn player after 3 seconds and add player respawning

--- World.cs
+++ World.cs
@@ -4,21 +4,23 @@ using System;
 
 public class World : Node2D {
 	public static readonly PackedScene PlayerScene = ResourceLoader.Load<PackedScene>("res://Player/Player.tscn");
-	public Vector2 _SpawnPoint = new Vector2(160, 96);
-	private Player _Player;
+	public Vector2 SpawnPoint = new Vector2(160, 96);
 	private HealthUi _HealthUi;
+	private Player _Player;
+	private Timer _SpawnTimer;
 	private YSort _YSort;
 
 	public override void _Ready() {
 		_HealthUi = GetNode<HealthUi>("CanvasLayer/HealthUi");
+		_SpawnTimer = GetNode<Timer>("SpawnTimer");
 		_YSort = GetNode<YSort>("YSort");
-		SpawnPlayer();
+		_SpawnTimer.Start();
 	}
 
 	public void SpawnPlayer() {
 		_Player = World.PlayerScene.Instance<Player>();
 		_Player.Connect(nameof(Player.Ready), this, "_OnPlayerReady");
-		_Player.Position = _SpawnPoint;
+		_Player.Position = SpawnPoint;
 		var cameraTransform = new RemoteTransform2D();
 		cameraTransform.RemotePath = "../../../Camera2D";
 		_Player.AddChild(cameraTransform);
@@ -26,6 +28,16 @@ public class World : Node2D {
 	}
 
 	private void _OnPlayerReady(Player player) {
+		_Player.Stats.Connect(nameof(Stats.Die), this, "_OnPlayerStatsDie");
 		_HealthUi.UiStats = _Player.Stats;
+	}
+
+	private void _OnPlayerStatsDie() {
+		_HealthUi.UiStats = null;
+		_SpawnTimer.Start();
+	}
+
+	private void _OnSpawnTimerTimeout() {
+		SpawnPlayer();
 	}
 }
